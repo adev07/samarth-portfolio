0450d724873d6c5ff87132657ff5ec1fcb5a3fef
adev07 <adityaanand1245@gmail.com>
2024-01-09 15:57:01 +0530

    verson2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,10 @@ import banner from "../../assets/images/image/banner.png";
 import project1 from "../../assets/images/image/project1.png";
 import project2 from "../../assets/images/image/project2.png";
 import project3 from "../../assets/images/image/project3.png";
+import gmail from "../../assets/images/icons/Icons Here.png";
+import linkdein from "../../assets/images/icons/linkedin (1).png";
 import { ROUTES } from "../../routes/RouterConfig";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -16,7 +19,7 @@ const Home = () => {
   return (
     <div className="bg-[#0D0D0D] min-h-[100vh]">
       <div className="flex flex-col items-center justify-center overflow-hidden">
-        <div className="text-center custom-tracking text-[200px] text-[#fff] opacity-[10%] leading-[240px] font-normal mt-[-6%]">
+        <div className="text-center custom-tracking text-[200px] text-[#fff] opacity-[10%] leading-[240px] font-normal mt-[-5%]">
           SAMARTH
         </div>
         <div className="flex flex-col items-center justify-center my-[64px]">
@@ -27,7 +30,7 @@ const Home = () => {
             Backend | Dev ops
           </div>
         </div>
-        <div className="text-center custom-tracking text-[200px] text-[#fff] opacity-[10%] leading-[240px] font-normal mb-[-6%]">
+        <div className="text-center custom-tracking text-[200px] text-[#fff] opacity-[10%] leading-[240px] font-normal mb-[-5%]">
           MANWANI
         </div>
       </div>
@@ -52,80 +55,110 @@ const Home = () => {
         </div>
       </div>
       <div className="pb-[440px]">
-        <div className="flex text-[#fff] items-center justify-center gap-[450px] custom-fonts">
+        <div className="flex text-[#fff] items-center justify-center gap-[300px] custom-fonts">
           <div className="text-center">
-            <div className="text-[20px] font-semibold">Autumn</div>
+            <div className="text-[20px] font-semibold">Packet Fanatic</div>
             <div className="text-[14px] font-normal leading-normal">
-              Jan 2015 - July 2018
+              Jan 2021 - Jun 2021
             </div>
           </div>
           <div className="text-center">
-            <div className="text-[20px] font-semibold">Google Inc.</div>
+            <div className="text-[20px] font-semibold">Samsung R&D</div>
             <div className="text-[14px] font-normal leading-normal">
-              March 2015 - Dec 2018
+              June 2021 - June 2022
             </div>
           </div>
           <div className="text-center">
             <div className="text-[20px] font-semibold">Zluri</div>
             <div className="text-[14px] font-normal leading-normal">
-              March 2015 - Dec 2018
+              Jul 2021 - Present
+            </div>
+          </div>
+          <div className="text-center">
+            <div className="text-[20px] font-semibold">Codeclock</div>
+            <div className="text-[14px] font-normal leading-normal">
+              Aug 2023 - Present
             </div>
           </div>
         </div>
         <div className="flex items-center justify-center mt-2">
           <div className="w-2 h-2 bg-white rounded-full"></div>
-          <div className="h-px bg-white w-[600px]"></div>
+          <div className="h-px bg-white w-[435px]"></div>
           <div className="w-2 h-2 bg-white rounded-full"></div>
-          <div className="h-px bg-white w-[600px]"></div>
+          <div className="h-px bg-white w-[435px]"></div>
+          <div className="w-2 h-2 bg-white rounded-full"></div>
+          <div className="h-px bg-white w-[435px]"></div>
           <div className="w-2 h-2 bg-white rounded-full"></div>
         </div>
       </div>
       <div className="flex items-center justify-center cursor-pointer">
         <div className="relative group">
-          <img src={project1} alt="" className="w-full h-full object-cover" />
-            <div className="text-[120px] font-normal custom-spacing absolute inset-0 bg-black opacity-50 text-[#fff] flex items-center justify-center">
-              onesta
-            </div>
-            <div className="opacity-0 group-hover:opacity-100 absolute text-white text-center px-4 bottom-72 left-24">
-              <ul className="list-disc">
-                <li>Description item 1</li>
-                <li>Description item 2</li>
-                <li>Description item 3</li>
-              </ul>
-            </div>
-        </div>
-        <div className="relative group">
-          <img src={project2} alt="" className="w-full h-full object-cover" />
+          <img
+            src={project1}
+            alt="Project 1"
+            className="w-[620px] h-[975px] grayscale transition duration-300 ease-in-out group-hover:grayscale-0"
+          />
           <div className="text-[120px] font-normal custom-spacing absolute inset-0 bg-black opacity-50 text-[#fff] flex items-center justify-center">
-            nexus
-          </div>
-          <div className="opacity-0 group-hover:opacity-100 absolute text-white text-center px-4 bottom-72 left-24">
-            <ul className="list-disc">
-              <li>Description item 1</li>
-              <li>Description item 2</li>
-              <li>Description item 3</li>
-            </ul>
+            Autumn
           </div>
         </div>
         <div className="relative group">
-          <img src={project3} alt="" className="w-full h-full object-cover" />
+          <img
+            src={project2}
+            alt="Project 2"
+            className="w-[620px] h-[975px] grayscale transition duration-300 ease-in-out group-hover:grayscale-0"
+          />
           <div className="text-[120px] font-normal custom-spacing absolute inset-0 bg-black opacity-50 text-[#fff] flex items-center justify-center">
-            Toquiri
+            HandsOn
           </div>
-          <div className="opacity-0 group-hover:opacity-100 absolute text-white text-center px-4 bottom-72 left-24">
-            <ul className="list-disc">
-              <li>Description item 1</li>
-              <li>Description item 2</li>
-              <li>Description item 3</li>
-            </ul>
+        </div>
+        <div className="relative group">
+          <img
+            src={project1}
+            alt="Project 2"
+            className="w-[620px] h-[975px] grayscale transition duration-300 ease-in-out group-hover:grayscale-0"
+          />
+          <div className="text-[120px] font-normal custom-spacing absolute inset-0 bg-black opacity-50 text-[#fff] flex items-center justify-center">
+            Autumn
           </div>
         </div>
       </div>
-
-      <div className="py-[252px] text-center">
-        <h3 className="text-[128px] custom-spacing text-[#fff]">
-          let’s connect?
-        </h3>
+      <div>
+        <div className="py-[90px] flex justify-between mx-[68px] items-center">
+          <div className="text-[#fff]">
+            <h3 className="text-[32px] custom-spacing-footer2">
+              Samarth Manwani
+            </h3>
+            <p className="custom-fonts text-[13px] opacity-[50%] custom-spacing-footer">
+              "Curiosity and consistency will always beat talent."
+            </p>
+          </div>
+          <div>
+            <div className="text-[#fff] pb-2 custom-fonts opacity-[50%] custom-spacing-footer">
+              Let's Connect
+            </div>
+            <div className="flex gap-4 cursor-pointer">
+              <div className="w-[30px] h-[30px] flex items-center justify-center bg-[#fff] bg-opacity-[10%] rounded-full">
+                <i className="text-[#fff] bi bi-google"></i>
+              </div>
+              <div className="w-[30px] h-[30px] flex items-center justify-center bg-[#fff] bg-opacity-[10%] rounded-full">
+                <i className="text-[#fff] bi bi-whatsapp"></i>
+              </div>
+              <div className="w-[30px] h-[30px] flex items-center justify-center bg-[#fff] bg-opacity-[10%] rounded-full">
+                <i class="text-[#fff] bi bi-linkedin"></i>
+              </div>
+              <div className="w-[30px] h-[30px] flex items-center justify-center bg-[#fff] bg-opacity-[10%] rounded-full">
+                <i class="text-[#fff] bi bi-github"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-start pb-[34px] mx-[66px] gap-4">
+          <div className="h-px bg-white w-full bg-opacity-[10%]"></div>
+          <p className="custom-fonts text-[13px] opacity-[50%] text-[#fff] custom-spacing-footer">
+            All right reversed, 2024
+          </p>
+        </div>
       </div>
     </div>
   );
