17ab072415f9e64e87571df14395c660397ffa3f
adev07 <adityaanand1245@gmail.com>
2024-01-10 11:03:33 +0530

    changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
+import { MenuProps } from "antd";
+import { Button, Dropdown } from "antd";
 import { useNavigate } from "react-router-dom";
 import banner from "../../assets/images/image/banner.png";
 import project1 from "../../assets/images/image/project1.png";
@@ -11,17 +13,74 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Home = () => {
   const navigate = useNavigate();
+  const aboutSectionRef = useRef(null);
+  const journeySectionRef = useRef(null);
+  const projectsSectionRef = useRef(null);
 
   const changeDir = (dir) => {
     navigate(dir);
   };
 
+  const handleAboutClick = () => {
+    aboutSectionRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  };
+
+  const handleJourneyClick = () => {
+    journeySectionRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  };
+
+  const handleProjectsClick = () => {
+    projectsSectionRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  };
+
   return (
-    <div className="bg-[#0D0D0D] min-h-[100vh]">
-      <div className="flex flex-col items-center justify-center overflow-hidden">
+    <div className="bg-[#0D0D0D] min-h-[100vh] scroll-smooth relative">
+      <div className="text-end z-[100] pt-8 mr-8 absolute top-1 right-1">
+        <Dropdown
+          placement="bottomLeft"
+          dropdownRender={() => (
+            <div className="bg-[#0D0D0D] text-white flex flex-col items-start">
+              <button
+                onClick={handleAboutClick}
+                className="py-[2px] custom-fonts"
+              >
+                About
+              </button>
+              <button
+                onClick={handleJourneyClick}
+                className="py-[2px] custom-fonts"
+              >
+                Journey
+              </button>
+              <button
+                onClick={handleProjectsClick}
+                className="py-[2px] custom-fonts"
+              >
+                Projects
+              </button>
+            </div>
+          )}
+        >
+          <div className="flex justify-center items-center text-[#fff] custom-fonts text-[14px] cursor-pointer">
+            <p>Introduction</p>
+            <div><i className="bi bi-caret-down-fill p-2"></i></div>
+          </div>
+        </Dropdown>
+      </div>
+      <div className="flex flex-col items-center justify-center overflow-hidden w-[100%] top-0 left-0">
         <div className="text-center custom-tracking text-[200px] text-[#fff] opacity-[10%] leading-[240px] font-normal mt-[-5%]">
           SAMARTH
         </div>
+
         <div className="flex flex-col items-center justify-center my-[64px]">
           <div>
             <img src={banner} alt="" />
@@ -34,7 +93,11 @@ const Home = () => {
           MANWANI
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center mx-[420px] py-[440px] font-normal">
+      <div
+        id="aboutSection"
+        ref={aboutSectionRef}
+        className="flex flex-col justify-center items-center mx-[420px] py-[400px] font-normal"
+      >
         <div className="text-[#fff] text-[128px] custom-spacing leading-[153.6px]">
           I’m samarth
         </div>
@@ -54,28 +117,28 @@ const Home = () => {
           dummy text ever since the 1500s.
         </div>
       </div>
-      <div className="pb-[440px]">
-        <div className="flex text-[#fff] items-center justify-center gap-[300px] custom-fonts">
+      <div id="journeySection" ref={journeySectionRef} className="py-[400px]">
+        <div className="flex text-[#fff] items-center justify-center gap-[260px] custom-fonts">
           <div className="text-center">
-            <div className="text-[20px] font-semibold">Packet Fanatic</div>
+            <div className="text-[16px] font-semibold">Packet Fanatic</div>
             <div className="text-[14px] font-normal leading-normal">
               Jan 2021 - Jun 2021
             </div>
           </div>
           <div className="text-center">
-            <div className="text-[20px] font-semibold">Samsung R&D</div>
+            <div className="text-[16px] font-semibold">Samsung R&D</div>
             <div className="text-[14px] font-normal leading-normal">
               June 2021 - June 2022
             </div>
           </div>
           <div className="text-center">
-            <div className="text-[20px] font-semibold">Zluri</div>
+            <div className="text-[16px] font-semibold">Zluri</div>
             <div className="text-[14px] font-normal leading-normal">
               Jul 2021 - Present
             </div>
           </div>
           <div className="text-center">
-            <div className="text-[20px] font-semibold">Codeclock</div>
+            <div className="text-[16px] font-semibold">Codeclock</div>
             <div className="text-[14px] font-normal leading-normal">
               Aug 2023 - Present
             </div>
@@ -83,15 +146,19 @@ const Home = () => {
         </div>
         <div className="flex items-center justify-center mt-2">
           <div className="w-2 h-2 bg-white rounded-full"></div>
-          <div className="h-px bg-white w-[435px]"></div>
+          <div className="h-px bg-white w-[380px]"></div>
           <div className="w-2 h-2 bg-white rounded-full"></div>
-          <div className="h-px bg-white w-[435px]"></div>
+          <div className="h-px bg-white w-[380px]"></div>
           <div className="w-2 h-2 bg-white rounded-full"></div>
-          <div className="h-px bg-white w-[435px]"></div>
+          <div className="h-px bg-white w-[380px]"></div>
           <div className="w-2 h-2 bg-white rounded-full"></div>
         </div>
       </div>
-      <div className="flex items-center justify-center cursor-pointer">
+      <div
+        id="projectsSection"
+        ref={projectsSectionRef}
+        className="flex items-center justify-center cursor-pointer"
+      >
         <div className="relative group">
           <img
             src={project1}
